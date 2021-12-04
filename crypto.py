730072a73a6ce38413141d7d430e0b0179e91c89
Scott Howard <scott@showard.ca>
2021-12-04 09:32:44 -0800

    major change. reworked X3DH exchange, added double ratchet encryptor, demo message sending

--- crypto.py
+++ crypto.py
@@ -5,16 +5,85 @@
 # 4. Encrypt a message/operate ratchet
 # 5. Decrypt a message/operate ratchet
 
+
+# Key generation libraries
 from cryptography.hazmat.primitives.asymmetric.ed25519 import *
 from cryptography.hazmat.primitives.asymmetric.x25519 import *
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+
+# Key translation to/from bytes
 from cryptography.hazmat.primitives import serialization
-from cryptography.hazmat.primitives import hashes
-from cryptography.exceptions import *
+
+# Key translation from ed25519 to x25519
 import nacl.signing
+
+# Key Derivation Function
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import hashes
+
+# Message encryption/decryption
+from cryptography.hazmat.primitives.ciphers import Cipher
+from cryptography.hazmat.primitives.ciphers import algorithms
+from cryptography.hazmat.primitives.ciphers import modes
+
+# Exceptions from invalid keys/signatures
+from cryptography.exceptions import *
+
+
+def diffieHellman(ipk_private = None, ipk_public = None, \
+                  epk_private = None, epk_public = None, \
+                  spk_private = None, spk_public = None, \
+                  opk_private = None, opk_public = None):
+    """Perform a Diffie-Hellman 4-way exchange with the given keys.
+
+    If Bob is seeking a shared secret with Alice, Bob will call this function
+    first with the following keys:
+    - ipk_private (Bob's)
+    - epk_private (Bob's)
+    - ipk_public  (Alice's)
+    - spk_public  (Alice's)
+    - opk_public  (Alice's)
+
+    Alice will call this function subsequently with the following keys:
+    - ipk_private (Alice's)
+    - spk_private (Alice's)
+    - opk_private (Alice's)
+    - ipk_public  (Bob's)
+    - epk_public  (Bob's)"""
+
+    # startHandshake
+    if epk_private is not None:
+        DH1 = ipk_private.exchange(spk_public)
+        DH2 = epk_private.exchange(ipk_public)
+        DH3 = epk_private.exchange(spk_public)
+        DH4 = epk_private.exchange(opk_public)
+
+    # finishHandshake
+    else:
+        DH1 = spk_private.exchange(ipk_public)
+        DH2 = ipk_private.exchange(epk_public)
+        DH3 = spk_private.exchange(epk_public)
+        DH4 = opk_private.exchange(epk_public)
+
+    return DH1 + DH2 + DH3 + DH4
+
+
+def kdf(key):
+    """Derive a hashed key of 32-byte length from a shared secret input."""
+
+    kdf = HKDF(\
+            algorithm=hashes.SHA256(), \
+            length = 32, \
+            salt=bytes(256), \
+            info=b'cryptomessenger')
+    return kdf.derive(key)
 
 
 class Server:
+    """Key Distribution Center.
+
+    Stores key bundles of users, and provides them to users looking to begin
+    a secret conversation with a user."""
+
     def __init__(self):
         self.users = {}
         self.nextUserID = 0
@@ -25,7 +94,7 @@ class Server:
         Returns a user ID."""
 
         kb = KeyBundle.unpackage(keybundle)
-        kb.peer_id = self.nextUserID
+        kb.id = self.nextUserID
         self.users[self.nextUserID] = kb
         self.nextUserID += 1
         return self.nextUserID - 1
@@ -35,7 +104,7 @@ class Server:
 
         if user_id in self.users.keys():
             kb = KeyBundle()
-            kb.peer_id = self.users[user_id].peer_id
+            kb.id = self.users[user_id].id
             kb.ipk = self.users[user_id].ipk
             kb.spk = self.users[user_id].spk
             kb.opk = self.users[user_id].opk.pop(0) # forget the sent OPK
@@ -62,7 +131,7 @@ class User:
         self.ipk = None
         self.spk = None
         self.opk = None
-        self.shared_keys = {}
+        self.ratchets = {}
 
     @staticmethod
     def new(): # -> User
@@ -83,90 +152,134 @@ class User:
 
         self.id = server.register(KeyBundle.from_user(self).package())
 
-    def initializeCommunication(self, keybundle_pkg: dict): # -> tuple
-        """Derive a shared secret from a KeyBundle.
+    def startHandshake(self, peer_keybundle: dict):
+        """Calculate the shared secret from a users prekey bundle."""
 
-        Returns a tuple of (ipk, epk, keybundle) to be sent to peer so the
-        shared secret can be generated independently."""
-
-        keybundle = KeyBundle.unpackage(keybundle_pkg)
+        kb = KeyBundle.unpackage(peer_keybundle)
         epk = KeyPair.generate()
 
-        # TODO: improve this. The key is stored in plaintext in memory.
-        # Instead of storing keys, we should store Encryptor objects
-        # from cryptography.hazmat.primitives.ciphers.Cipher
-        # Left as is for rough initial proof of concept
-        self.shared_keys[keybundle.peer_id] = (
-                self.diffieHellman(self.ipk, epk, keybundle), 
-                self.ipk.public_bytes(), 
-                epk.public_bytes())
+        if self.id is None:
+            raise Exception('Register with Server first.')
+
+        self.ratchets[peer_keybundle['ID']] = DoubleRatchet(
+            my_id = self.id,
+            peer_id = kb.id,
+            associated_data = self.ipk.public_bytes() + kb.ipk.public_bytes(),
+            root_key = diffieHellman(
+                ipk_private = self.ipk,
+                epk_private = epk,
+                ipk_public = kb.ipk,
+                spk_public = kb.spk,
+                opk_public = kb.opk))
         epk.private = None
 
-        return (self.ipk.public_bytes(), epk.public_bytes(), keybundle.package())
+        return {'id': self.id, \
+                'ipk': self.ipk.public_bytes(), \
+                'epk': epk.public_bytes(), \
+                'opk': kb.opk.public_bytes()}
 
-        # TODO: replace the above with something like this:
-        #associated_data = self.ipk.public_bytes() + keybundle.ipk.public_bytes()
-        #encryptor = Cipher(
-        #        algorithms.AES(kdf.derive(DH1 + DH2 + DH3 + DH4)),
-        #        modes.GCM(b'U\xa4\xdb\xee\x96A\xb4\xf4\xa0\x98\xbf\xe1') # IV
-        #    ).encryptor()
-        #encryptor.authenticate_additional_data(associated_data)
-        #self.encryptors[keybundle.peer_id] = (encryptor, associated_data)
+    def finishHandshake(self, peer_keybundle: dict):
+        """Calculate the matching shared secret from a users prekey bundle."""
 
-    # TODO: finish this
-    def encrypt(self, user: int, message: str) -> bytes:
-        """Encrypt a message to a user with a shared key."""
+        peer_id = peer_keybundle['id']
+        peer_ipk = IPK.from_public_bytes(peer_keybundle['ipk'])
+        peer_epk = KeyPair.from_public_bytes(peer_keybundle['epk'])
+        used_opk = OPK.from_public_bytes(peer_keybundle['opk'])
 
-        if user in self.encryptors.keys():
-            encryptor, associated_data = self.encryptors[id]
-            ciphertext = encryptor.update(message) + encryptor.finalize()
-            return (ciphertext, encryptor.tag)
-        else:
-            raise Exception # user not found
+        opk = None
+        for i in range(len(self.opk)):
+            if self.opk[i].public_bytes() == used_opk.public_bytes():
+                opk = self.opk.pop(i)
+                break
 
-    # TODO: finish this
-    def decrypt(key, associated_data, ciphertext, tag):
-        pass
+        self.ratchets[peer_id] = DoubleRatchet(
+            my_id = self.id,
+            peer_id = peer_id,
+            associated_data = peer_ipk.public_bytes() + self.ipk.public_bytes(),
+            root_key = diffieHellman(
+                ipk_public = peer_ipk,
+                epk_public = peer_epk,
+                ipk_private = self.ipk,
+                spk_private = self.spk,
+                opk_private = opk))
 
-    def diffieHellman(self, ipk, epk, keybundle):
-        """Derive the shared secret from keys."""
 
-        # Initial diffie hellman
-        if isinstance(ipk.private, Ed25519PrivateKey):
-            DH1 = ipk.exchange(keybundle.spk)
-            DH2 = epk.exchange(keybundle.ipk)
-            DH3 = epk.exchange(keybundle.spk)
-            DH4 = epk.exchange(keybundle.opk)
+class DoubleRatchet:
+    """Double ratchet encryption
 
-        # Peer has generated shared secret, so generate the same.
-        else:
-            matchingOPK = None
-            for opk in self.opk:
-                if opk.public_bytes() == keybundle.opk.public_bytes():
-                    matchingOPK = opk
-                    break
-            if matchingOPK is None:
-                raise Exception # couldn't find matching OPK
+    Creates three ratchet: the first is the root key, seeded with the shared
+    secret derived during the diffie-hellman key exchange with the prekey
+    bundles. The other two are the sending and receiving ratchets, which will
+    be turned everytime a message is encrypted or decrypted. Periodically,
+    they will be reset by ratching the root key."""
 
-            DH1 = self.spk.exchange(ipk)
-            DH2 = self.ipk.exchange(epk)
-            DH3 = self.spk.exchange(epk)
-            DH4 = matchingOPK.exchange(epk)
+    def __init__(self, root_key, my_id, peer_id, associated_data):
+        self.root_key = kdf(root_key)
+        self.my_id = my_id
+        self.peer_id = peer_id
+        self.associated_data = associated_data
+        self.recv_key = self.root_key
+        self.send_key = self.root_key
 
-        # Derive shared secret from above diffie-hellmans
-        kdf = HKDF(\
-                algorithm=hashes.SHA256(), \
-                length = 32, \
-                salt=bytes(256), \
-                info=b'cryptomessenger')
-        return kdf.derive(DH1 + DH2 + DH3 + DH4)
+        self.updateEncryptor()
+        self.updateDecryptor()
+
+    def updateRootKey(self):
+        """Ratchet the root key, and update the send and receive ratchets.
+
+        Provides perfect forward secrecy."""
+
+        self.root_key = kdf(self.root_key)
+        self.recv_key = self.root_key
+        self.send_key = self.root_key
+
+        self.updateEncryptor()
+        self.updateDecryptor()
+
+    def updateEncryptor(self):
+        """Refresh the encryptor with the ratcheted send key."""
+
+        self.send_key = kdf(self.send_key + str(self.my_id).encode())
+        self.encryptor = Cipher(
+            algorithms.AES(self.send_key),
+            modes.GCM(self.associated_data) # IV
+        ).encryptor()
+        self.encryptor.authenticate_additional_data(self.associated_data)
+
+    def updateDecryptor(self):
+        """Refresh the decryptor with the ratcheted receive key."""
+
+        self.recv_key = kdf(self.recv_key + str(self.peer_id).encode())
+        self.decryptor = Cipher(
+            algorithms.AES(self.recv_key),
+            modes.GCM(self.associated_data) # IV
+        ).decryptor()
+        self.decryptor.authenticate_additional_data(self.associated_data)
+
+    def encrypt(self, message):
+        """Encrypt a message and ratchet the send key."""
+
+        self.updateEncryptor()
+
+        return (self.encryptor.update(message.encode()) + self.encryptor.finalize(), self.encryptor.tag)
+
+    def decrypt(self, tagged_message):
+        """Decrypt a message and ratchet the receive key."""
+
+        message = tagged_message[0]
+        tag = tagged_message[1]
+
+        self.updateDecryptor()
+
+        text = self.decryptor.update(message) + self.decryptor.finalize_with_tag(tag)
+        return text.decode()
 
 
 class KeyBundle:
     """A KeyBundle for deriving shared secrets using X3DH."""
 
     def __init__(self):
-        self.peer_id = None
+        self.id = None
         self.ipk = None
         self.spk = None
         self.opk = None
@@ -189,7 +302,7 @@ class KeyBundle:
             for opk in self.opk:
                 packagedOPK.append(opk.public_bytes())
 
-        return {'ID'       : self.peer_id, \
+        return {'ID'       : self.id, \
                 'IPK'      : self.ipk.public_bytes(), \
                 'SPK'      : self.spk.public_bytes(), \
                 'signature': self.spk.signature, \
@@ -229,11 +342,11 @@ class KeyBundle:
             raise TypeError(f'invalid key bundle. Expected dictionary, got {type(bundle)}')
 
     @staticmethod
-    def from_keys(peer_id: int, ipk, spk, opk): # -> KeyBundle
+    def from_keys(id: int, ipk, spk, opk): # -> KeyBundle
         """Generate a KeyBundle from key objects."""
 
         kb = KeyBundle()
-        kb.peer_id = peer_id
+        kb.id = id
         kb.ipk = ipk
         kb.spk = spk
         kb.opk = opk
@@ -247,7 +360,7 @@ class KeyBundle:
         """Generate a KeyBundle from a User object."""
 
         kb = KeyBundle()
-        kb.peer_id = user.id
+        kb.id = user.id
         kb.ipk = user.ipk
         kb.spk = user.spk
         kb.opk = user.opk
@@ -261,7 +374,7 @@ class KeyPair:
     """A generic private/public key pair. 
 
     May contain X25519 or Ed25519 keys. This class is extended by
-    IPK/SPK/OPK/EDPK, but is used directly for ephemeral keys.
+    IPK/SPK/OPK, but is used directly for ephemeral keys.
 
     Important attributes:
     private -- The private key object.
@@ -463,13 +576,13 @@ class SPK(KeyPair):
         return spk
 
     @staticmethod
-    def from_private_bytes(private_bytes: bytes): # -> SPK
+    def from_private_bytes(private_bytes: bytes, signature: bytes): # -> SPK
         """Create an SPK object from private key bytes representation."""
 
         spk = SPK()
         kp = KeyPair.from_private_bytes(private_bytes)
         spk.private, spk.public = kp.private, kp.public
-        # TODO what about signature?
+        spk.signature = signature
 
         return spk
 
@@ -529,19 +642,47 @@ if __name__ == '__main__':
 
     print('Alice registers with the server.')
     alice.register(server)
-    print("Alice's user id is now {alice.id}")
+    print(f"Alice's user id is now {alice.id}")
+
+    print('Bob registers with the server.')
+    bob.register(server)
+    print(f"Bob's user id is now {bob.id}")
 
     print("Bob wants to communicate with Alice, so he asks the server for her keybundle.")
-    alice_kb = server.getKeyBundle(alice.id)
+    alice_kb = server.getKeyBundle(alice.id).package()
 
     print("Bob will receive Alice's key bundle and derive the shared secret.")
-    keys_for_alice = bob.initializeCommunication(alice_kb.package())
+    handshake_keys = bob.startHandshake(alice_kb)
 
     print('Bob sends an initial message to Alice, including the required public keys for her to generate the shared secret.')
-    key=alice.diffieHellman(
-        IPK.from_public_bytes(keys_for_alice[0]),
-        KeyPair.from_public_bytes(keys_for_alice[1]), 
-        KeyBundle.unpackage(keys_for_alice[2])
-        )
+    alice.finishHandshake(handshake_keys)
 
-    print(f'Did alice derive the same secret key as bob? {key == bob.shared_keys[0][0]}')
+    print(f'Did alice derive the same secret key as bob? {alice.ratchets[bob.id].root_key == bob.ratchets[alice.id].root_key}')
+
+    plaintext='attack at dawn'
+    print(f'Bob sends Alice a message: {plaintext}')
+    cryptext=bob.ratchets[alice.id].encrypt(plaintext)
+    print(f'cryptext is: {cryptext}')
+    print(f'Alice sees: {alice.ratchets[bob.id].decrypt(cryptext)}')
+
+    plaintext='affirmative'
+    print(f'Alice responds to Bob: {plaintext}')
+    cryptext=alice.ratchets[bob.id].encrypt(plaintext)
+    print(f'cryptext is: {cryptext}')
+    print(f'Bob sees: {bob.ratchets[alice.id].decrypt(cryptext)}')
+
+    print(f'Alice and Bob reset their ratchets')
+    alice.ratchets[bob.id].updateRootKey()
+    bob.ratchets[alice.id].updateRootKey()
+
+    plaintext='attack at dawn'
+    print(f'Bob sends Alice a message: {plaintext}')
+    cryptext=bob.ratchets[alice.id].encrypt(plaintext)
+    print(f'cryptext is: {cryptext}')
+    print(f'Alice sees: {alice.ratchets[bob.id].decrypt(cryptext)}')
+
+    plaintext='affirmative'
+    print(f'Alice responds to Bob: {plaintext}')
+    cryptext=alice.ratchets[bob.id].encrypt(plaintext)
+    print(f'cryptext is: {cryptext}')
+    print(f'Bob sees: {bob.ratchets[alice.id].decrypt(cryptext)}')
